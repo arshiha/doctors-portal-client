4e37825a1a9b6ffdf8561a7f6a9f1f7edabf67a3
arshiha bristy <arshihabristy5@gmail.com>
2022-05-09 23:37:25 +0600

    service section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner.js";
 import Info from "./Info.js";
+import Services from "./Services.js";
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-12">
       <Banner></Banner>
       <Info></Info>
+      <Services></Services>
     </div>
   );
 };
