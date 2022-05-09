1dda4660a1ff948ab9b5e47900aed7dcbb40cea0
arshiha bristy <arshihabristy5@gmail.com>
2022-05-09 23:11:34 +0600

    infoCard added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner.js";
+import Info from "./Info.js";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Info></Info>
     </div>
   );
 };
