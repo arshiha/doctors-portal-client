c2bfcc7d09c3632e34268078cb6ca6e66f7d33b1
arshiha bristy <arshihabristy5@gmail.com>
2022-05-09 22:36:44 +0600

    banner complete

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -2,18 +2,27 @@ import React from "react";
 import chair from "../../assets/images/chair.png";
 
 const Banner = () => {
+  const myStyle = {
+    backgroundImage: "url('https://i.ibb.co/s2mVMkr/bg.png')",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
   return (
-    <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
-        <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button class="btn btn-primary">Get Started</button>
+    <div style={myStyle}>
+      <div class="hero min-h-screen px-12">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p class="py-6">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the
+            </p>
+            <button class="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
+              Get Started
+            </button>
+          </div>
         </div>
       </div>
     </div>
